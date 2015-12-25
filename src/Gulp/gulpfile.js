a4cc220f58532b8dfd725e8dba162a65c473075d
amman vedi <amman.vedi@pulselive.com>
2015-12-25 00:34:08 +0000

    create cleaner version of code

--- src/Gulp/gulpfile.js
+++ src/Gulp/gulpfile.js
@@ -3,12 +3,6 @@ var print = require('gulp-print');
 var Documental = require('Documental');
 
 
-//irb-web/widgets/mc-new/src/js/components/insights/MatchTeamsInAttackCard.js
-
-//../../../irb-web/**/*.js
 gulp.task('default', function () {
-    gulp.src([ '../../../irb-web/widgets/mc-new/src/js/components/insights/MatchTeamsInAttackCard.js' ,'!../../../irb-web/tools/**', '!../../../irb-web/common/src/lib/3rd-party/**', '!../../../irb-web/**/**.min.js', '!../../../irb-web/storyboards/**'])
-        .pipe(Documental.UTIL.ingest()).pipe(print(function(filepath) {
-            return " Finished Parsing : " + filepath + " - ( Overall " + Documental.UTIL.getTotal() +" Methods, " + Math.floor(Documental.UTIL.getPercentDocumented()) +"% Documented) \n\n";
-        }));
+    gulp.src([ '../../../pulse-js/src/**/*.js']).pipe(Documental.UTIL.ingest("pulse-js")).on( 'finish', function(){ Documental.UTIL.exportDataToJSON();});
 });
