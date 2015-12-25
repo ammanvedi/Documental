85a9a9be832eb17b83d61a63bf16c8f9be62ebc6
amman vedi <amman.vedi@pulselive.com>
2015-12-25 00:56:18 +0000

    single line vars and create doc gulp task

--- src/Gulp/gulpfile.js
+++ src/Gulp/gulpfile.js
@@ -3,6 +3,6 @@ var print = require('gulp-print');
 var Documental = require('Documental');
 
 
-gulp.task('default', function () {
+gulp.task('docs', function () {
     gulp.src([ '../../../pulse-js/src/**/*.js']).pipe(Documental.UTIL.ingest("pulse-js")).on( 'finish', function(){ Documental.UTIL.exportDataToJSON();});
 });
