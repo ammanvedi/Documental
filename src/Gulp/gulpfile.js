6621f84376700ce8cffe26f84510b093c49a28a3
amman vedi <amman.vedi@pulselive.com>
2016-01-05 02:14:27 +0000

    return stream

--- src/Gulp/gulpfile.js
+++ src/Gulp/gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var Documental = require( 'Documental' );
 
 gulp.task('docs', function () {
-    gulp.src([ 'test-projects/pulse-js/src/**/*.js']).pipe(Documental());
+    return gulp.src([ 'test-projects/pulse-js/src/**/*.js']).pipe(Documental());
 });
