852bc40e512ac93c937f852c1099aaa6bec8837f
amman vedi <amman.vedi@pulselive.com>
2016-01-24 20:28:21 +0000

    add namespacing as a fallback to direct inference

--- src/Gulp/gulpfile.js
+++ src/Gulp/gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var Documental = require( 'Documental' );
 
 gulp.task('docs', function () {
-    return gulp.src([ 'test-projects/premier-league-web/src/**/*.js']).pipe(Documental());
+    return gulp.src([ 'test-projects/premier-league-web/src/widgets/sites/pl/list-filter/js/filter-controller/**/*.js']).pipe(Documental());
 });
